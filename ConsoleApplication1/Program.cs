befc409a74b50dd602c08c421202c968a87de3fe
metjka <metka506@gmail.com>
2016-05-21 21:57:23 +0200

    console

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QUIZ;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+            Service1 service1 = new Service1();
+            Console.WriteLine(service1.HelloWorld());
+
+
+            Console.ReadKey();
+        }
+    }
+}
